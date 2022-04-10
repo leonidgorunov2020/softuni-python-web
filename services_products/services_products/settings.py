c55d0e99d042096f7f199999bae689fb1ca6bf9c
Leonid Gorunov <leo@wppotion.com>
2022-04-10 22:49:45 +0300

    Fixed static files settings

--- services_products/services_products/settings.py
+++ services_products/services_products/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from coverage.python import os
+
 from services_products import services
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,8 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+BASE_DIR_2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR_2, 'static'),
+)
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'staticfiles',
 # ]
