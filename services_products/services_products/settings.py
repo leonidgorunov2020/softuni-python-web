9c3e998a43a658fdb4058c67caf7506b70437eb0
Leonid Gorunov <leo@wppotion.com>
2022-04-10 22:01:28 +0300

    Static files update for heroku only!

--- services_products/services_products/settings.py
+++ services_products/services_products/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'staticfiles',
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = 'media/'
