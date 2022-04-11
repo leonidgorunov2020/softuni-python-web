0b2d02e37a862d98439745e04e925cc6968d753f
Leonid Gorunov <leo@wppotion.com>
2022-04-12 00:05:49 +0300

    Updated profile management in Django admin

--- services_products/services_products/accounts/admin.py
+++ services_products/services_products/accounts/admin.py
@@ -5,4 +5,4 @@ from services_products.accounts.models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    fields = ('first_name', 'last_name', 'email')
+    list_display = ('first_name', 'last_name', 'email')
