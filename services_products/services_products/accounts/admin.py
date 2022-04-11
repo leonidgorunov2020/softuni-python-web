8f5384ba76e05c9314581871da3288d9dc67f1f8
Leonid Gorunov <leo@wppotion.com>
2022-04-12 00:11:06 +0300

    Updated profile management presentation in Django admin

--- services_products/services_products/accounts/admin.py
+++ services_products/services_products/accounts/admin.py
@@ -5,4 +5,8 @@ from services_products.accounts.models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email')
+    list_display = ('upper_case_name',)
+
+    @admin.display(description='Name')
+    def upper_case_name(self, obj):
+        return ("%s %s" % (obj.first_name, obj.last_name)).upper()
