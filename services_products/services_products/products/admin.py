0174ddeb5feb65573ba77235a6832e431b7f7912
Leonid Gorunov <leo@wppotion.com>
2022-04-12 00:35:35 +0300

    Added services presenation in Django admin

--- services_products/services_products/products/admin.py
+++ services_products/services_products/products/admin.py
@@ -6,4 +6,3 @@ from services_products.products.models import Product
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'date_of_release', 'price')
-    
